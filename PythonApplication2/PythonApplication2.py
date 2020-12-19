b0ef7fda55226f5bd6fb8d9c77100512c10f1dd0
Mohamed N. Gaafar <46086535+mo-gaafar@users.noreply.github.com>
2020-12-19 10:08:33 +0200

    Low Pass Filter Attempted

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets, uic, QtCore
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit, QFileDialog, QScrollBar
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit, QFileDialog, QScrollBar, QComboBox
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
 import numpy as np
@@ -8,7 +8,7 @@ import os
 import csv
 from random import randint
 import wfdb
-
+from scipy.signal import butter,filtfilt
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -22,15 +22,22 @@ class MainWindow(QtWidgets.QMainWindow):
         #self.setCentralWidget(self.graphWidget)
         self.filename = None
         self.filterStatus = 0
-        #If No file opened, plot random points
+       #initializing the 4 Plotting lines
+
         self.x = list(range(20))  # 20 time points
         self.y =[ 1 for _ in range(20)]  # 100 data points
+        
+        self.x1 = list(range(20))
+        self.y1 = [ 1 for _ in range(20)]
 
-        #p1 = win.addPlot(title="Basic array plotting", y=np.random.normal(size=100))
+        self.x2 = list(range(20))
+        self.y2 = [ 1 for _ in range(20)]
 
-   
+        self.x3 = list(range(20))
+        self.y3 = [ 1 for _ in range(20)]
+
         #Anti aliasing for prettier lines
-        pg.setConfigOptions(antialias = True)
+        #pg.setConfigOptions(antialias = True)
 
         #Set Background
         self.graphWidget.setBackground('w')
@@ -40,10 +47,19 @@ class MainWindow(QtWidgets.QMainWindow):
         self.graphWidget.setLabel("bottom", "bottom text", **styles)
 
         pen = pg.mkPen(color=(255, 0, 0), width = 1)
-        
-        self.data_line =  self.graphWidget.plot(self.x, self.y, pen=pen)
-        #timer to update plot every 10ms
 
+        #Plot Lines
+        self.data_line =  self.graphWidget.plot(self.x, self.y, pen=pen) #Orignial Data
+        self.band_line = self.graphWidget.plot(self.x1, self.y1, pen = pg.mkPen(color = (0,0,255),width = 1)) #Band Pass  filter line
+        self.low_line = self.graphWidget.plot(self.x2, self.y2, pen = pg.mkPen(color = (0,0,255),width = 1)) #Low Pass filter line
+        self.high_line = self.graphWidget.plot(self.x3, self.y3, pen = pg.mkPen(color = (0,0,255),width = 1)) #High Pass filter line
+
+        #Hiding Filter Lines
+        self.band_line.hide()
+        self.low_line.hide()
+        self.high_line.hide()
+
+        #timer to update plot every 10ms
         self.timer = QtCore.QTimer()
         self.timer.setInterval(10)
         self.timer.timeout.connect(self.update_plot_data)
@@ -64,11 +80,23 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ZoominCount = 0
         self.graphWidget.setLimits(xMin = 0)
         self.sigLength = 100
-        #self.buttonpause = self.findChild(QPushButton, "pauseButton")
-        #self.buttonpause.clicked.connect(self.clickedBtn)
+        self.filterIndex = 0
+
+        # Filter requirements.
+        self.T = 5         # Sample Period
+        self.fs = 300.0       # sample rate, Hz
+        self.cutoff = 100     # desired cutoff frequency of the filter, Hz ,      slightly higher than actual 1.2 Hz
+        self.nyq = 0.5 * self.fs  # Nyquist Frequency
+        self.order = 2       # sin wave can be approx represented as quadratic
+        self.n = int(self.T * self.fs) # total number of samples
 
         self.buttonplay = self.findChild(QPushButton, "playButton")
         self.buttonplay.clicked.connect(self.clickedpBtn)
+
+        self.buttonFilter = self.findChild(QPushButton, "filterButton")
+        self.buttonFilter.clicked.connect(self.clickedfilterBtn)
+
+        self.comboFilter = self.findChild(QComboBox, "comboBox")
 
         self.buttonbrowse = self.findChild(QPushButton, "browseButton")
         self.buttonbrowse.clicked.connect(self.clickedbBtn)
@@ -99,6 +127,36 @@ class MainWindow(QtWidgets.QMainWindow):
     def sliderval2(self):
         print(self.horizontalScrollBar.value())
         self.graphWidget.setRange(xRange= ((self.CounterX/100)*self.horizontalScrollBar.value(),(self.CounterX/100)*self.horizontalScrollBar.value()+100))
+    
+    def plot_hide(self): #Shows the relevant Data Lines
+       if self.filterIndex == 0:
+            self.data_line.show()
+            self.band_line.hide()
+            self.low_line.hide()
+            self.high_line.hide()
+
+       if self.filterIndex == 1:
+            self.data_line.hide()
+            self.band_line.show()
+            self.low_line.hide()
+            self.high_line.hide()
+
+       if self.filterIndex == 2:
+            self.data_line.hide()
+            self.band_line.hide()
+            self.low_line.show()
+            self.high_line.hide()
+
+       if self.filterIndex == 3:
+            self.data_line.hide()
+            self.band_line.hide()
+            self.low_line.hide()
+            self.high_line.show()
+            
+    def clickedfilterBtn(self): #Shows Desired Filter Plot
+        self.filterIndex = self.comboFilter.currentIndex()
+        print("filter number:",self.filterIndex," applied!")
+        self.plot_hide()
 
     def clickedpBtn(self):
       
@@ -116,25 +174,36 @@ class MainWindow(QtWidgets.QMainWindow):
             self.timer.stop() 
 
         self.show()
+
+    def butter_lowpass_filter(self,data): #Butterworth Filter
+        self.normal_cutoff = self.cutoff / self.nyq
+        # Get the filter coefficients 
+        b,a = butter(self.order, self.normal_cutoff, btype='lowpass', analog=False)
+        y = filtfilt(b, a, data)
+        return y
         
     def clickedbBtn(self): #Browse Button triggered
         self.filename = QFileDialog.getOpenFileName()[0]
         print(self.filename)
 
         self.filename = self.filename[:-4] #removes .hea ending
-
+        #Original Signal Processing, ADC
         self.record = wfdb.rdrecord(self.filename )
         self.d_signal = self.record.adc()
         self.record.adc(inplace = True)
         self.MaxY = max(self.d_signal[:][0]) #Maximum Y Value in the ECG File
         self.MinY = min(self.d_signal[:][0]) #Minimum Y Value in the ECG File
 
-        print(self.record.sig_len) #Prints the length of the selected signal
+        #Low Pass filter
+        self.low_signal = self.butter_lowpass_filter(self.d_signal)
+
+        print(self.record.sig_len) #Prints the length of the Original signal
         self.sigLength = self.record.sig_len #init signal length variable
 
         self.CounterX = 0 #resets array counter whenever a new file is selected
 
         self.show
+    
 
     def clickedzoominBtn(self):
         print("zoomin")
@@ -147,7 +216,6 @@ class MainWindow(QtWidgets.QMainWindow):
     def clickedzoomoutBtn(self):
         print("zoomout")
         self.ZoominCount = 0
-        #self.graphWidget.setRange(xRange= (0,1000))
         self.graphWidget.autoRange()
 
     def clickedFFTBtn(self):  
@@ -162,32 +230,36 @@ class MainWindow(QtWidgets.QMainWindow):
             self.graphWidget.getPlotItem().ctrl.fftCheck.setChecked(False)  
             self.graphWidget.autoRange()
             self.graphWidget.setAutoPan(x=100, y=None)
-    
-    def update_plot_data(self):
-       if self.filename == None:
-        # self.x = self.x[1:]  # Remove the first y element.
-        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
 
-       # self.y = self.y[1:]  # Remove the first  x element
+    
+    def update_plot_data(self): #Live Data Plotting
+       if self.filename == None: #if no file is imported
+
+        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+        self.x1.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+        self.x2.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+        self.x3.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+
+
         self.y.append(1)  # Add a new random value.
         self.data_line.setData(self.x, self.y)  # Update the data.
+
 
        else:
         self.CounterX +=1
         self.x.append(self.x[-1] + 1)
-        
+        self.x2.append(self.x2[-1] + 1)
 
-        if self.sigLength > self.CounterX: #if signal array is not fully plotted do this
-            if self.filterStatus == 0:
+        if self.sigLength > self.CounterX: #Updates 4 Lines, unfiltered, Smooth Filter, Low Pass Filter, High Pass Filter
                 self.y.append(self.record.d_signal[self.CounterX][0])
                 self.data_line.setData(self.x,self.y)
+
+                #self.y1.append(self.smoothfiltered)
+                #self.smooth_line.setData(self.x1,self.y1)
+                self.y2.append(self.low_signal[self.CounterX][0])
+                self.low_line.setData(self.x2,self.y2)
+        
             
-            #if self.filterStatus == 1:
-            
-            #if self.filterStatus == 2:
-                
-            #if self.filterStatus == 3:
-                
 def main():
     app = QtWidgets.QApplication(sys.argv)
     main = MainWindow()
